f08a6a1a532474b887fa69ea801acb285aa0c1d5
AmpsCoursera <87051282+AmpsCoursera@users.noreply.github.com>
2021-07-08 15:49:00 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,19 @@
-makeVector <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {
   humps <- NULL
   set <- function(y) {
     x <<- y
     humps <<- NULL
   }
   get <- function() x
-  setInverse <- function(inverse) humps <<- mean
+  setInverse <- function(inverse) humps <<- inverse
   getInverse <- function() humps
   list(set = set, get = get,
        setInverse = setInverse,
        getInverse = getInverse)
 }
-cacheInverse <- function(x, ...) {
-  humps <- x$getmean()
+
+cacheSolve <- function(x, ...) {
+  humps <- x$getInverse()
   if(!is.null(humps)) {
     message("getting cached data")
     return(humps)
